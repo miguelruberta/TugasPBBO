4562c6e46a7b351dfa67853a7750db9fbfbe14f8
Kevin Hartono <kevin.hartono1868@gmail.com>
2020-11-11 13:51:45 +0700

    Enterprise Architect
    Added previous details that include:
    - Enterprise Architect File
    - JAVA File
    - UI Design

---
+++ Enterprise Architect/Toko Sablon/Sales Department/CustomerPayment.java
@@ -0,0 +1,27 @@
+package entity;
+
+import Customer.entity.Customer;
+
+/**
+ * @author Kevin Hartono
+ * @version 1.0
+ * @created 26-Oct-2020 07:37:39
+ */
+public class CustomerPayment {
+
+	private Customer customer;
+	private String id;
+	private SalesInvoice invoice;
+	private int jumlah;
+	private String method;
+	public SalesInvoice m_SalesInvoice;
+
+	public CustomerPayment(){
+
+	}
+
+	public void finalize() throws Throwable {
+
+	}
+
+}
---
+++ Enterprise Architect/Toko Sablon/Vendor/SizeKaos.java
@@ -0,0 +1,25 @@
+package Vendor.entity;
+
+
+/**
+ * @author Kevin Hartono
+ * @version 1.0
+ * @created 26-Oct-2020 07:38:45
+ */
+public class SizeKaos {
+
+	private char id;
+	private double lebar_dada;
+	private double panjang_badan;
+	private double panjang_lengan;
+	public Kaos m_Kaos;
+
+	public SizeKaos(){
+
+	}
+
+	public void finalize() throws Throwable {
+
+	}
+
+}
